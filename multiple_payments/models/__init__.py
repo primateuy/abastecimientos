6986c5fcfd4061d4449c8add081dfbccd64e7625
primateuy <diego@primate.uy>
2025-08-26 20:58:12 -0300

    _create_payment_acount'
    Operación no válida
    
    'mps.payment.aggregator' object has no attribute '_create_payment_acount'

--- multiple_payments/models/__init__.py
+++ multiple_payments/models/__init__.py
@@ -8,3 +8,4 @@ from . import account_payment
 from . import account_move_line_payment_aggregator
 from . import aggregator_buttons
 from . import aggregator_invoices
+from . import aggregator_helpers
