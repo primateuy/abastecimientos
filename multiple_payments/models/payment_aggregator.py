8ad957de44129a71c40228ac8ba2845d197006ac
primateuy <diego@primate.uy>
2025-08-26 19:03:03 -0300

    fix transf

--- multiple_payments/models/payment_aggregator.py
+++ multiple_payments/models/payment_aggregator.py
@@ -148,7 +148,7 @@ class PaymentAggregator(models.Model):
                 vals.update({
                     'date': line.date,
                     'is_internal_transfer': True,
-                    'payment_type': 'outbound',
+                    'payment_type': 'transfer',
                     'ref': _('Internal Transfer'),
                     'payment_method_id': line.payment_method_id.id if line.payment_method_id else False,
                 })
