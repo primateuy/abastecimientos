d9c40396c9e60a45c47ab3eeffa3320f9980cb28
Andres Iglesias <102163345+aiglesas@users.noreply.github.com>
2025-08-26 15:13:23 -0300

    Add files via upload

---
+++ sale_company_product_domain/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sale: filtrar productos por compañía de la venta",
+    "version": "17.0.1.0.0",
+    "author": "TuNombre",
+    "license": "LGPL-3",
+    "depends": [
+        "sale_management",
+        "product_multi_company"
+    ],
+    "data": [
+        "views/sale_order_views.xml"
+    ],
+    "installable": true
+}
