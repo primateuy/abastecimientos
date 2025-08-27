8fbadff002eaea7262901e8f3dc0eb801978599c
primateuy <diego@primate.uy>
2025-08-27 09:53:29 -0300

    securtiy fix

---
+++ mps_payment_security_fix/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{'name': "MPS Payment Aggregator - Security/Create Fix",
+'summary': "Permite crear agregadores multiempresa y fuerza company_id en create.",
+'version': "16.0.20250827.0",
+'category': "Accounting",
+'depends': ["base", "account", "multiple_payments"],
+'data': ["security/ir.model.access.csv", "security/rules.xml"],
+'installable': true,
+'application': false,
+'license': "LGPL-3"}
---
+++ mps_payment_security_fix/models/__init__.py
@@ -0,0 +1,1 @@
+from . import aggregator_fix
