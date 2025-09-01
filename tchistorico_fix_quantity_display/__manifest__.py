790ed587fd10386492632989035c10b8b559966f
primateuy <diego@primate.uy>
2025-09-01 03:31:44 -0300

    fix quantity display tchistorico

---
+++ tchistorico_fix_quantity_display/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "TChistorico - Fix quantity_display for SVL (v17)",
+    "summary": "Evita el error en el reporte de Valoración al añadir el campo faltante 'quantity_display'.",
+    "version": "17.0.1.0.0",
+    "author": "ChatGPT (fix for Diego)",
+    "license": "LGPL-3",
+    "depends": ["stock_account", "tchistorico"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
