5209310cf762d51eecd4c11169fe2511c82a9d07
aiglesas <aiglesias.giordano@gmail.com>
2025-09-29 13:20:57 -0300

    fix manifest

--- ar_currency_rate_import/__manifest__.py
+++ ar_currency_rate_import/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "AR Currency Rate Import",
     "summary": "Permite importar la tasa de moneda argentina en facturas",
-    "version": "17",
+    "version": "17.0",
     "author": "iglesias-andres - Primate Uy",
     "license": "LGPL-3",
     "depends": ["account"],
